0e6706139c9be38544f0c95530cbdd31b5bbd833
Pradipta Sarma <38849757+pradiptasarma@users.noreply.github.com>
2020-01-24 16:43:36 +0530

    require basic auth for every request (#2)

--- src/main/java/com/pradipta/todo/security/SecurityConfig.java
+++ src/main/java/com/pradipta/todo/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
@@ -25,6 +26,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests()
                 .antMatchers("/**")
                 .hasAnyRole("ADMIN", "USER")
