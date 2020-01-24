5600f0c33ce7c7827ce8499f2fd32d00c7513f18
Pradipta Sarma <pradipta.sarma@UE-B-NOTE-0046.local>
2020-01-24 17:18:04 +0530

    retrieving username from basic auth

--- src/main/java/com/pradipta/todo/task/TaskController.java
+++ src/main/java/com/pradipta/todo/task/TaskController.java
@@ -3,8 +3,10 @@ package com.pradipta.todo.task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
@@ -13,28 +15,32 @@ public class TaskController {
     private TaskService service;
 
     @RequestMapping("/tasks")
-    public List<Task> getAllTask(){
+    public List<Task> getAllTask(HttpServletRequest req){
+        String user = req.getUserPrincipal().getName();
         return service.getAllTask();
     }
 
     @RequestMapping("/tasks/pending")
-    public List<Task> getPendingTask(){
+    public List<Task> getPendingTask(HttpServletRequest req){
+        String user = req.getUserPrincipal().getName();
         return service.getPendingTask();
     }
 
     @RequestMapping("/tasks/completed")
-    public List<Task> getCompletedTask(){
+    public List<Task> getCompletedTask(HttpServletRequest req){
+        String user = req.getUserPrincipal().getName();
         return service.getCompletedTask();
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/tasks")
-    public Task addTask(@RequestBody Task task){
+    public Task addTask(@RequestBody Task task, HttpServletRequest req){
+        String user = req.getUserPrincipal().getName();
         return service.addTask(task);
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "/tasks/markDone/{id}")
-    public ResponseEntity<?> markDone(@PathVariable String id){
+    public ResponseEntity<?> markDone(@PathVariable String id, HttpServletRequest req){
+        String user = req.getUserPrincipal().getName();
         return new ResponseEntity(service.markDone(id), HttpStatus.OK);
-        //return service.markDone(id);
     }
 }
