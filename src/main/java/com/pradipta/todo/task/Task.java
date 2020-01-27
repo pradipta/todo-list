867e4fd41f7e38078f04c08e7d25eca7359d3e0b
Pradipta Sarma <38849757+pradiptasarma@users.noreply.github.com>
2020-01-27 14:48:44 +0530

    created at (#5)

--- src/main/java/com/pradipta/todo/task/Task.java
+++ src/main/java/com/pradipta/todo/task/Task.java
@@ -1,11 +1,15 @@
 package com.pradipta.todo.task;
 
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 @NoArgsConstructor
@@ -19,8 +23,10 @@ public class Task {
 
     private String text;
     private Boolean isDone = Boolean.FALSE;
+    @CreationTimestamp
+    private LocalDateTime creationDate;
 
-    public Task(int id, String text){
+    public Task(int id, String text, Date date){
         this.id = (long)id;
         this.text = text;
         this.isDone = Boolean.FALSE;
