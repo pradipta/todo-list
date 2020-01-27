5dd0ebfd7584a9be19b474931cc7faf407c1ed02
Pradipta Sarma <pradipta.sarma@UE-B-NOTE-0046.local>
2020-01-27 13:07:30 +0530

    id as integer and auto generated

--- src/main/java/com/pradipta/todo/task/Task.java
+++ src/main/java/com/pradipta/todo/task/Task.java
@@ -3,18 +3,25 @@ package com.pradipta.todo.task;
 import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 @NoArgsConstructor
+@Getter
+@Setter
 public class Task {
-    @Id
-    @Getter @Setter private String id;
-    @Getter @Setter private String text;
-    @Getter @Setter private Boolean isDone = Boolean.FALSE;
 
-    public Task(String id, String text){
-        this.id = id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String text;
+    private Boolean isDone = Boolean.FALSE;
+
+    public Task(int id, String text){
+        this.id = (long)id;
         this.text = text;
         this.isDone = Boolean.FALSE;
     }
