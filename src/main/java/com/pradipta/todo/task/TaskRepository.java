0dd2b78f0b9c2fd56c6f8c88c25ba29023f3b632
Pradipta Sarma <38849757+pradiptasarma@users.noreply.github.com>
2020-01-28 13:44:22 +0530

    long too int (#7)

--- src/main/java/com/pradipta/todo/task/TaskRepository.java
+++ src/main/java/com/pradipta/todo/task/TaskRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Integer> {
     //GetTAsks
     //GetPendingTasks
     //GetCompletedTasks
